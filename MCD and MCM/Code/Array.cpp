359d664c8f5bad58ce6faa02ad6264483a9484fc
CrisdeckESPE <6015322+CrisdeckESPE@users.noreply.github.com>
2020-06-02 09:04:59 -0500

    MCM and MCD added

---
+++ MCD and MCM/Code/Array.cpp
@@ -0,0 +1,105 @@
+/***********************************************************************
+ * Module:  Array.cpp
+ * Author:  Chicaiza Jaya Cristopher Jennsy 
+ * Modified: Tuesday, June 2, 2020 8:56:55 AM
+ * Purpose: Implementation of the class Array
+ ***********************************************************************/
+
+#include "Array.h"
+#include <cstdlib>
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+#include <stdio.h>
+#include <ctype.h>
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Array::Array()
+// Purpose:    Implementation of Array::Array()
+// Return:     
+////////////////////////////////////////////////////////////////////////
+
+Array::Array()
+{
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Array::wax(int * v, int dim)
+// Purpose:    Implementation of Array::wax()
+// Parameters:
+// - v
+// - dim
+// Return:     void
+////////////////////////////////////////////////////////////////////////
+
+void Array::wax(int * v, int dim)
+{
+   for(int i=0;i<dim;i++)
+	*(v+i)=0;
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Array::print(int * v, int dim)
+// Purpose:    Implementation of Array::print()
+// Parameters:
+// - v
+// - dim
+// Return:     void
+////////////////////////////////////////////////////////////////////////
+
+void Array::print(int * v, int dim)
+{
+   for(int i=0;i<dim;i++){
+	printf("%d,",*(&(*v)));
+	*(v++);
+//	cout<<v[i]<<", ";
+	}
+	printf("\n");
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Array::enter(int * v, int dim)
+// Purpose:    Implementation of Array::enter()
+// Parameters:
+// - v
+// - dim
+// Return:     void
+////////////////////////////////////////////////////////////////////////
+
+void Array::enter(int * v, int dim)
+{
+   for(int i=0;i<dim;i++){
+		cout<<"Valor "<<i+1<<endl;
+	//	scanf("%d",*(&v)+i);
+		cin>>*(v+i);
+	}
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Array::mcm(int * v, int dim)
+// Purpose:    Implementation of Array::mcm()
+// Parameters:
+// - v
+// - dim
+// Return:     int
+////////////////////////////////////////////////////////////////////////
+
+int Array::mcm(int * v, int dim)
+{
+   // TODO : implement
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Array::mcd(int * v, int dim)
+// Purpose:    Implementation of Array::mcd()
+// Parameters:
+// - v
+// - dim
+// Return:     int
+////////////////////////////////////////////////////////////////////////
+
+int Array::mcd(int * v, int dim)
+{
+   // TODO : implement
+}
---
+++ MCD and MCM/Code/MCD and MCM.cpp
@@ -0,0 +1,37 @@
+/******************************************************************
+*            UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE              *
+* CARRERA: Ingenieria de Software                                 *
+* SEMESTRE: Tercer semestre                                       *
+* MATERIA: Estructura de Datos                                    *
+* NRC: 6396                                                       *
+* AUTOR: Chicaiza Jaya Cristopher Jennsy                          *
+* VERSION DEL PROGRAMA: 1.0                                       *
+******************************************************************/
+#include <cstdlib>
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "Array.cpp"
+
+int main(int argc, char *argv[])
+{
+	int d;
+	Array objArray;
+	printf("Ingrese el tamaño del array:	");
+	scanf("%d",&d);
+	int *v=new int[d];
+	objArray.wax(v,d);
+	objArray.print(v,d);
+	system("pause");
+	printf("\nIngrese los valores del array:\n");
+	objArray.enter(v,d);
+	objArray.print(v,d);
+	system("pause");
+	objArray.print(v,d);
+	//delete (v); // libera memoriapero en mmalloc
+	free (v); //libera el espacio
+	system("pause");
+	return 0;
+}
