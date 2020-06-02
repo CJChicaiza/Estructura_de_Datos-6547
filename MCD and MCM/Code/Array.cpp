131b50440af4aee5dbefc2a7280ef4613d4ead12
CrisdeckESPE <6015322+CrisdeckESPE@users.noreply.github.com>
2020-06-02 12:22:56 -0500

    Add MCM

--- MCD and MCM/Code/Array.cpp
+++ MCD and MCM/Code/Array.cpp
@@ -12,6 +12,7 @@
 #include <conio.h>
 #include <stdio.h>
 #include <ctype.h>
+#define MAX 999
 using namespace std;
 
 ////////////////////////////////////////////////////////////////////////
@@ -87,8 +88,58 @@ void Array::enter(int * v, int dim)
 
 int Array::mcm(int * v, int dim)
 {
+	int i,j,k,h,w,p;
+	h=0;
+    int **multiplym=(int **)malloc(dim*sizeof(int *));
+    for(j=0;j<dim;j++)
+        *(multiplym+j)=(int *)malloc(MAX*sizeof(int));
 
-   
+    int f1, f2, f3=1;
+    for(i=0;i<dim;i++)
+    {
+         for(j=0;j<MAX;j++)
+         {
+             f1 = *(v+i);
+             f2 = f1 * f3;
+             f3++;
+            *(*(multiplym+i)+j) = f2;
+         }
+         f3=1;
+    }
+    int firstm = *(*(multiplym+0)+h);
+    for(i=1;i<dim;)
+    {
+        for(j=0;j<MAX;)
+        {
+            if(firstm == *(*(multiplym+i)+j))
+            {
+                w = firstm;
+                i++;
+                j=0;
+            }
+            else
+            {
+                j++;
+                p = j;
+            }
+            if((j == MAX)  &&  (h != MAX))
+            {
+                h++;
+                firstm = *(*(multiplym+0)+h);
+                i=1;
+                j=0;
+            }
+            if((h == MAX-1) && (j == p))
+            {
+                printf("\nNo se puede calcular el mcm o es demasiado grande\n");
+                return 0;
+            }
+            if((i == dim) && (firstm == w))
+            {
+                return firstm;
+            }
+        }
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -102,7 +153,11 @@ int Array::mcm(int * v, int dim)
 
 int Array::mcd(int * v, int dim)
 {
-    int num, res, mcd;
+	int num, res, mcd;
+	if ((dim==2)&&((*(v)%*(v+1))!=0)&&((*(v+1)%*(v))!=0)){
+		return 1;
+	}
+	else{
  	for (int i=0;i<dim;i++){
         if (i == 1) {
             mcd = *(v+i);
@@ -114,6 +169,7 @@ int Array::mcd(int * v, int dim)
                 *(v+i) = res;
             } while (res != 0);
         }
+	}
 	}
 	return mcd;
 }
--- MCD and MCM/Code/MCD and MCM.cpp
+++ MCD and MCM/Code/MCD and MCM.cpp
@@ -24,16 +24,15 @@ int main(int argc, char *argv[])
 	int *v=new int[d];
 	objArray.wax(v,d);
 	objArray.print(v,d);
-	system("pause");
 	
 	printf("\nIngrese los valores del array:\n");
 	objArray.enter(v,d);
 	objArray.print(v,d);
 	system("pause");
 	
+	mcm=objArray.mcm(v,d);
 	mcd=objArray.mcd(v,d);
-	printf("El mcd es %d\n", mcd);
-	
+	printf("El mcd es:	%d\nY el mcm es:	%d\n", mcd, mcm);
 	free (v); //libera el espacio
 	system("pause");
 	return 0;
